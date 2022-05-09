2ac6aad23a1ce0991c8365b338b70974f20d04f5
GianITS <gianluca.mattiello.studio@fitstic-edu.com>
2022-05-09 18:57:18 +0200

    connessione fra pagine

--- website/views.py
+++ website/views.py
@@ -5,3 +5,15 @@ views = Blueprint('views', __name__)
 @views.route('/HomePage')
 def home():
     return render_template('homepage.html')
+
+@views.route('/Clienti')
+def clients():
+    return render_template('clients.html')
+
+@views.route('/Immobili')
+def properties():
+    return render_template('properties.html')
+
+@views.route('/RichiesteImmobiliari')
+def properties_request():
+    return render_template('properties_request.html')
