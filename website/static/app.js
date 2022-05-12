0649e99de5723779e1ed3f4482cd8029c9f666cd
GianITS <gianluca.mattiello.studio@fitstic-edu.com>
2022-05-12 19:45:50 +0200

    modifiche grafiche

--- website/static/app.js
+++ website/static/app.js
@@ -1,7 +1,11 @@
 const btnInsert = document.getElementById("btninsert")
 const dpList = document.getElementById("dplist")
+const spanIns1 = document.querySelector(".spanIns1")
+const spanIns2 = document.querySelector(".spanIns2")
 
 btnInsert.addEventListener('click', function(){
     dpList.classList.toggle("hidden");
-    console.log("premuto")
+    spanIns1.classList.toggle("animate");
+    spanIns2.classList.toggle("animate");
+    btnInsert.classList.toggle("active");
 })
