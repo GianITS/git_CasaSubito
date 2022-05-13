267b7a483b0637f688d088cbce741b6f478958fd
GianITS <gianluca.mattiello.studio@fitstic-edu.com>
2022-05-13 11:05:55 +0200

    sistemata la session e il menu utente

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,10 @@
 from crypt import methods
-from flask import Blueprint, render_template, redirect, request, url_for, session
+from logging import exception
+from flask import Blueprint, flash, render_template, redirect, request, url_for, session
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
-from .models import agents_collection, Agent
+from .models import agents_collection
 
 auth = Blueprint('auth', __name__)
 
@@ -15,15 +16,29 @@ class FormLogin(FlaskForm):
 
 @auth.route('/', methods=["GET", "POST"])
 def login():
-    utente = None
-    password = None
+    utente = ""
+    password = ""
     form=FormLogin()
     if form.validate_on_submit():
         utente = form.utente.data
         password = form.password.data
-        session["agent"] = agents_collection.find_one({"username":utente, "password":password},{"_id":0, "nome":1})
-        if session["agent"] != "" or session["agent"] != None:
-            print(session["agent"]["nome"])
+        try:
+            session["agent"] = agents_collection.find_one({"username":utente, "password":password},{"_id":0, "nome":1})['nome']
+            form.utente.data = ""
+            form.password.data = ""
             return redirect(url_for("views.home"))
-        form.utente.data = None
+        except:
+            form.utente.data = ""
+            form.password.data = ""
+            flash("Utente non registrato")
+        #     session["agent"] = None
+        # if session["agent"] != None:
+        #     form.utente.data = ""
+        #     form.password.data = ""
+        #     return redirect(url_for("views.home"))
+        # else:
+        #     form.utente.data = ""
+        #     form.password.data = ""
+        #     flash("Utente non registrato")
+    
     return render_template('login.html',form=form, utente=utente, password=password)
